55a8856a7061720f0f7ff1c93cf88f18ea5b16fa
Victor Manuel Freixas Lemus <victormanuelfreixaslemus@vcv064192.vpn.uci.edu>
2025-02-28 09:59:14 -0800

    Function added to get the molecular orbital matrix from closed shell computations.

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from calc_AO_overlap import calc_AO_overlap
+from get_MO_matrix_closed_shell import get_MO_matrix_closed_shell
 import numpy as np
 
+'''
 # Testing the AO overlpas
 
 file1 = 'geometry.xyz'
@@ -8,3 +10,13 @@ file1 = 'geometry.xyz'
 AO_overlaps = calc_AO_overlap(file1, 'def2-TZVP')
 
 np.savetxt('ao_overlap.txt', AO_overlaps, fmt = '%.6f')
+'''
+
+# Testing reading the MO matrix
+
+moldenFile = 'HBQ_Neutral.molden'
+Nbf = 519
+
+MO_matrix = get_MO_matrix_closed_shell(moldenFile, Nbf)
+
+np.savetxt('mo_matrix.txt', MO_matrix, fmt = '%.6f')
