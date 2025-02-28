3243260dd81a76ee10bf8c5752ff8dd156f858fc
Victor Manuel Freixas Lemus <victormanuelfreixaslemus@vcv064192.vpn.uci.edu>
2025-02-28 11:37:43 -0800

    The function "get_CI_open_shell" was added to read the standard output from a NWChem open shell computation and return the CI coefficients corresponding to a specified channel 'alpha' or 'beta'

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from calc_AO_overlap import calc_AO_overlap
 from get_MO_matrix_closed_shell import get_MO_matrix_closed_shell
 from get_MO_matrix_open_shell import get_MO_matrix_open_shell
+from get_CI_open_shell import get_CI_open_shell
 import numpy as np
 
 '''
@@ -24,6 +25,7 @@ MO_matrix = get_MO_matrix_closed_shell(moldenFile, Nbf)
 np.savetxt('mo_matrix.txt', MO_matrix, fmt = '%.6f')
 '''
 
+'''
 # Testing reading the MO matrix (closed shell)
 
 moldenFile = 'HBQ_Cation_O.molden'
@@ -32,3 +34,14 @@ Nbf = 519
 MO_matrix = get_MO_matrix_open_shell(moldenFile, Nbf, 'alpha')
 
 np.savetxt('mo_matrix.txt', MO_matrix, fmt = '%.6f')
+'''
+
+# Testing reading the CI vectors for open shell
+
+nwFile = 'tddft_Cation_O.out'
+root = 1
+channel = 'beta'
+CI_vector = get_CI_open_shell(nwFile, root, channel)
+
+print('CI_vector: ')	
+print(CI_vector)
