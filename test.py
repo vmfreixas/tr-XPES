27dcf2a9b65e582d515ff31c7371af841780a98f
Victor Manuel Freixas Lemus <victormanuelfreixaslemus@vcv076123.vpn.uci.edu>
2025-03-04 12:38:48 -0800

    Function "calc_AO_overlap" name updated to "calc_AO_overlap_from_xyz".
      Function "get_AO_overlap" added to read atomic orbital overlaps from
    NWChem standard output.
    
      Fortran subroutines added to read molecular orbital vectors written
    in binary from NWChem with extensions ".movecs".
    
      Notes on the molecular orbital files from NWWChem. The molecular
    orbitals written in the ".molden" have some non-trivial swaps for
    compatibility purpurses with other software and are not consisten with
    the atomic orbital overlaps written in the standard output. In order
    to use molecular orbitals and atomic orbital overlaps consistent with
    each other, it is better to read the basis information from the
    ".molden" files and calculate the atomic orbital overlaps with this
    information.

--- test.py
+++ test.py
@@ -1,16 +1,28 @@
+from get_AO_overlap import get_AO_overlap
 from calc_AO_overlap import calc_AO_overlap
 from get_MO_matrix_closed_shell import get_MO_matrix_closed_shell
 from get_MO_matrix_open_shell import get_MO_matrix_open_shell
 from get_CI_closed_shell import get_CI_closed_shell
 from get_CI_open_shell import get_CI_open_shell
+from calc_MO_overlap import calc_MO_overlap
+from renormalize_MO import renormalize_MO
 import numpy as np
 
 '''
-# Testing the AO overlpas
+# Testing the calculation of AO overlaps with PYSCF
 
 file1 = 'geometry.xyz'
 
 AO_overlaps = calc_AO_overlap(file1, 'def2-TZVP')
+
+np.savetxt('ao_overlap.txt', AO_overlaps, fmt = '%.6f')
+'''
+
+'''
+# Testing reading the AO overlaps from NWChem standard output
+
+file1 = 'tddft_janpa.out'
+AO_overlaps = get_AO_overlap(file1)
 
 np.savetxt('ao_overlap.txt', AO_overlaps, fmt = '%.6f')
 '''
@@ -37,6 +49,7 @@ MO_matrix = get_MO_matrix_open_shell(moldenFile, Nbf, 'alpha')
 np.savetxt('mo_matrix.txt', MO_matrix, fmt = '%.6f')
 '''
 
+'''
 # Testing reading the CI vectors for a closed shell
 
 nwFile = 'tddft_Neutral.out'
@@ -45,6 +58,7 @@ CI_vector = get_CI_closed_shell(nwFile, root)
 
 print('CI_vector')
 print(CI_vector)
+'''
 
 '''
 # Testing reading the CI vectors for open shell
@@ -57,4 +71,32 @@ CI_vector = get_CI_open_shell(nwFile, root, channel)
 print('CI_vector: ')	
 print(CI_vector)
 '''
+
+# Testing the calculation of MO overlaps
+
+file1 = 'geometry.xyz'
+AO_overlaps = calc_AO_overlap(file1, 'def2-TZVP')
+np.savetxt('ao_overlap_pyscf.txt', AO_overlaps, fmt = '%.8f')
+
+
+file1 = 'tddft_6_31G.out'
+AO_overlaps = get_AO_overlap(file1)
+np.savetxt('ao_overlap.txt', AO_overlaps, fmt = '%.8f')
+
+Nbf = 243
+
+moldenFile = 'HBQ_Neutral_janpa_6_31G.molden'
+MO_matrix1 = get_MO_matrix_closed_shell(moldenFile, Nbf)
+np.savetxt('mo1.txt', MO_matrix1, fmt = '%.8f')
+
+#moldenFile = 'HBQ_Neutral_janpa.molden'
+#MO_matrix2 = get_MO_matrix_open_shell(moldenFile, Nbf, 'beta')
+#print(MO_matrix2)
+##MO_matrix2 = renormalize_MO(MO_matrix, AO_overlaps)
+
+#np.savetxt('mo2.txt', MO_matrix2, fmt = '%.6f')
+
+#MO_overlaps = calc_MO_overlap(MO_matrix1, MO_matrix2, AO_overlaps)
+
+#np.savetxt('mo_overlap.txt', MO_overlaps, fmt = '%.6f')
 
