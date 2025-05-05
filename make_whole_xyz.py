fb57c50bd4be618ac97df1d910a64f2fbd6d1d9c
vmfreixas <vmfreixas@gmail.com>
2025-05-05 10:17:04 -0700

    Writing the wrapper. Function 'make_whole_xyz' for generating the  whole coordinates xyz file created.

---
+++ make_whole_xyz.py
@@ -0,0 +1,22 @@
+#
+# This function read several xyz and concatenate them
+#
+
+import numpy as np
+
+def make_whole_xyz(masterDataFileName):
+    dirlist = []
+    with open(masterDataFileName + '/dirlist', 'r') as dFile:
+        for line in dFile:
+            dirlist.append(line.split()[0])
+    xyzAll = []
+    for d in dirlist:
+        with open(masterDataFileName + '/Neutral/' + d + '/coords.xyz') as xyzFile:
+            for line in xyzFile:
+                xyzAll.append(line)
+    with open(masterDataFileName + '/wholeCoords.xyz', 'w') as oFile:
+        for line in xyzAll:
+            oFile.write(line)
+#Example of use
+masterDataFileName = '/Users/victormanuelfreixaslemus/Desktop/Projects/Photoelectron_spectroscopy/tr_XPES_code/tr-XPES/data_test_2/'
+make_whole_xyz(masterDataFileName)
--- run_matlab.py
+++ run_matlab.py
@@ -11,7 +11,7 @@ eng.addpath(matlabFolder, nargout=0)
 eng.case0(nargout = 0)
 
 inputFileName = 'transition_moments/testdyson.mat'
-r = 1
+r = 1  #trajectory number within case0.m
 integralDir = 'transition_moments/dysonorbint'
 e0 = 0.1  #eV
 de = 0.1  #eV
@@ -20,6 +20,6 @@ eng.test_dysonint(inputFileName, r, integralDir, e0, ef, de, nargout = 0)
 
 orbIntDir = integralDir
 DOFileName = 'DO_AO_'
-nDO = 1
+nDO = 1 # Label for the Dyson orbital file
 outputDir = '/Users/victormanuelfreixaslemus/Desktop/Projects/Photoelectron_spectroscopy/tr_XPES_code/tr-XPES/Dipole_results/'
 eng.calc_dysondipintloop_lebedev74rod(orbIntDir, DOFileName, nDO, outputDir, e0, ef, de, nargout = 0)
