e32641350c73eedfc5b094b6a727a208a95afd17
vmfreixas <vmfreixas@gmail.com>
2026-02-09 09:08:37 -0800

    Dyson orbitals calculation from the GS added.
        In the subroutine "calc_DO_single_state", the case where the neutral
    molecule state is the GS has been added as the case where iNeutral is
    zero.

--- src/calc_DO_single_state.py
+++ src/calc_DO_single_state.py
@@ -15,7 +15,6 @@ from src.get_AO_overlap_from_NWChem import get_AO_overlap_from_NWChem
 from src.get_MO_matrix_from_NWChem import get_MO_matrix_from_NWChem 
 from src.get_CI_closed_shell import get_CI_closed_shell
 from src.get_CI_open_shell import get_CI_open_shell
-from src.select_state_from_cation import select_state_from_cation
 import numpy as np
 import time
 
@@ -37,53 +36,69 @@ def calc_DO_single_state(neutralFile, cationFile, Nbf, Nocc, iNeutral, state, ch
     moOverlap = moC.T @ ao @ moN
     np.savetxt('MO_287.dat', moOverlap, fmt = '%.16f')
 
-#   CI vector for the Neutral molecule
-    ciN = get_CI_closed_shell(neutralFile, iNeutral)
-
-#   Cation state
-    bAO = np.zeros((Nbf), dtype = float)
-    for root in [state - 1]:
-        print('Calculating DO for root ' + str(root + 1))
-        # CI vector for the Cation molecule
-        ciC = get_CI_open_shell(cationFile, root + 1, channel)
-        # Calculating Dyson orbitals from an excited state of the 
-        # neutral to an excited state of the Cation in the MO basis
-        bMO = np.zeros((Nbf), dtype = float)
-        for ia in range(len(ciN)):
-            for kb in range(len(ciC)):
-                for q in range(Nocc):
-                    i = ciN[ia][0] - 1
-                    a = ciN[ia][1] - 1
-                    k = ciC[kb][0] - 1
-                    b = ciC[kb][1] - 1
-                    if i != q:
-                        MO_swapped = np.copy(moOverlap)
-                        MO_swapped[[k, b], :] = MO_swapped[[b, k], :]
-                        MO_swapped[:, [i, a]] = MO_swapped[:, [a, i]]
-                        MO_swapped = np.delete(MO_swapped[:Nocc, :Nocc], q, axis = 1)
-                        bMO[q] += ciN[ia][2] * ciC[kb][2] * (-1) ** (Nbf + q) * (np.linalg.det(MO_swapped[:Nocc - 1, :Nocc - 1]))
-        for ia in range(len(ciN)):
-            for kb in range(len(ciC)):
-                for q in range(Nocc, Nbf):
-                    if ciN[ia][1] - 1 == q:
-                        j = ciN[ia][0] - 1
+    if iNeutral > 0:
+#       CI vector for the Neutral molecule
+        ciN = get_CI_closed_shell(neutralFile, iNeutral)
+#       Cation state
+        bAO = np.zeros((Nbf), dtype = float)
+        for root in [state - 1]:
+            print('Calculating DO for root ' + str(root + 1))
+            # CI vector for the Cation molecule
+            ciC = get_CI_open_shell(cationFile, root + 1, channel)
+            # Calculating Dyson orbitals from an excited state of the 
+            # neutral to an excited state of the Cation in the MO basis
+            bMO = np.zeros((Nbf), dtype = float)
+            for ia in range(len(ciN)):
+                for kb in range(len(ciC)):
+                    for q in range(Nocc):
+                        i = ciN[ia][0] - 1
+                        a = ciN[ia][1] - 1
                         k = ciC[kb][0] - 1
                         b = ciC[kb][1] - 1
-                        MO_swapped = np.copy(moOverlap)
-                        MO_swapped[[k, b], :] = MO_swapped[[b, k], :]
-                        MO_swapped = np.delete(MO_swapped[:Nocc, :Nocc], j, axis = 1)
-                        bMO[q] += ciN[ia][2] * ciC[kb][2] * (-1) ** (Nbf + j) * (np.linalg.det(MO_swapped[:Nocc - 1, :Nocc -1]))
-        # Transforming the Dyson orbitals to AO basis
-        bAO = moN @ bMO
-        print('And the norm is: ' + str(np.linalg.norm(bAO)))
+                        if i != q:
+                            MO_swapped = np.copy(moOverlap)
+                            MO_swapped[[k, b], :] = MO_swapped[[b, k], :]
+                            MO_swapped[:, [i, a]] = MO_swapped[:, [a, i]]
+                            MO_swapped = np.delete(MO_swapped[:Nocc, :Nocc], q, axis = 1)
+                            bMO[q] += ciN[ia][2] * ciC[kb][2] * (-1) ** (Nbf + q) * (np.linalg.det(MO_swapped[:Nocc - 1, :Nocc - 1]))
+            for ia in range(len(ciN)):
+                for kb in range(len(ciC)):
+                    for q in range(Nocc, Nbf):
+                        if ciN[ia][1] - 1 == q:
+                            j = ciN[ia][0] - 1
+                            k = ciC[kb][0] - 1
+                            b = ciC[kb][1] - 1
+                            MO_swapped = np.copy(moOverlap)
+                            MO_swapped[[k, b], :] = MO_swapped[[b, k], :]
+                            MO_swapped = np.delete(MO_swapped[:Nocc, :Nocc], j, axis = 1)
+                            bMO[q] += ciN[ia][2] * ciC[kb][2] * (-1) ** (Nbf + j) * (np.linalg.det(MO_swapped[:Nocc - 1, :Nocc -1]))
+    else: # The neutral molecule state is in the GS
+        bAO = np.zeros((Nbf), dtype = float)
+        for root in [state - 1]:
+            print('Calculating DO for root ' + str(root + 1))
+            # CI vector for the Cation molecule
+            ciC = get_CI_open_shell(cationFile, root + 1, channel)
+            # Neutral to an excited state of the Cation in the MO basis
+            bMO = np.zeros((Nbf), dtype = float)
+            for kb in range(len(ciC)):
+                for q in range(Nocc):
+                    k = ciC[kb][0] - 1
+                    b = ciC[kb][1] - 1
+                    MO_swapped = np.copy(moOverlap)
+                    MO_swapped[[k, b], :] = MO_swapped[[b, k], :]
+                    MO_swapped = np.delete(MO_swapped[:Nocc, :Nocc], q, axis = 1)
+                    bMO[q] += ciC[kb][2] * (-1) ** (Nbf + q) * (np.linalg.det(MO_swapped[:Nocc - 1, :Nocc - 1]))
+    # Transforming the Dyson orbitals to AO basis 
+    bAO = moN @ bMO
+    print('And the norm is: ' + str(np.linalg.norm(bAO)))
     end = time.time()
     print('done in ' + str(end - start) + ' seconds!')
     #print('The norm of the Dyson orbital is: ' + str(np.linalg.norm(bAO)))
     return(bAO)
 '''
 #Example of application
-neutralFile = 'Neutral/step_182/tddft.out'
-cationFile = 'Cation_O/step_182/tddft.out'
+neutralFile = '../examples/HBQ/tr_001/Neutral/step_01_0/tddft.out'
+cationFile = '../examples/HBQ/tr_001/Cation_O/step_01_0/tddft.out'
 Nbf = 519 
 Nocc = 51
 iNeutral = 1
